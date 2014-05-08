e7008318ecbf4dfb9a4162ca6ed1ef5cc960d4d4
Brandon Invergo <brandon@invergo.net>
2014-05-08 11:47:21 +0100

    clean up the base64 md5 handling to use more Python functions

--- seqdepot.py
+++ seqdepot.py
@@ -38,11 +38,11 @@ API_URL = 'http://seqdepot.net/api/v1'
 VERSION = '0.01'
 
 
-def _aseq_id_from_base64(bytes64):
+def _aseq_id_from_md5_bytes(md5_bytes):
     """Convert a base 64 bytes object to an aseq ID."""
 
-    trans_table = bytes64.maketrans(b'/+', b'_-')
-    aseq_id = bytes64.translate(trans_table, b'=\n')
+    md5_bytes64 = base64.urlsafe_b64encode(md5_bytes)
+    aseq_id = md5_bytes64.replace(b'=', b'')
     return aseq_id.decode('utf-8')
 
 
@@ -56,8 +56,8 @@ def aseq_id_from_md5_hex(md5_hex):
         aseqId
     """
     md5_bytes = binascii.unhexlify(md5_hex)
-    md5_bytes64 = base64.encodebytes(md5_bytes)
-    return _aseq_id_from_base64(md5_bytes64)
+    aseq_id = _aseq_id_from_md5_bytes(md5_bytes)
+    return aseq_id
 
 
 def aseq_id_from_sequence(sequence):
@@ -72,9 +72,10 @@ def aseq_id_from_sequence(sequence):
     Returns:
         aseqId
     """
-    md5sum = hashlib.md5(sequence.replace('-', '').encode('utf-8'))
-    md5_bytes64 = base64.encodebytes(md5sum.digest())
-    return _aseq_id_from_base64(md5_bytes64)
+    md5 = hashlib.md5(sequence.replace('-', '').encode('utf-8'))
+    md5_bytes = md5.digest()
+    aseq_id = _aseq_id_from_md5_bytes(md5_bytes)
+    return aseq_id
 
 
 def clean_sequence(sequence):
